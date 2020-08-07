d409d7aaa11fc7d118f4c5ba1a34fb68c4bea76b
femibams <femibams@gmail.com>
2020-08-07 14:02:30 +0100

    completed the loan application page

--- src/app/account/loan-application/loan-application.component.ts
+++ src/app/account/loan-application/loan-application.component.ts
@@ -9,6 +9,71 @@ import {NgbRatingConfig, NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 })
 export class LoanApplicationComponent implements OnInit {
   model: NgbDateStruct;
+  loanResultDiv: boolean = false
+
+  divs = [
+    {
+      id: 0,
+      name: 'start',
+      status: true
+    },
+    {
+      id: 1,
+      name: 'personal info',
+      status: false
+    },
+    {
+      id: 2,
+      name: 'financial info',
+      status: false
+    },
+    {
+      id: 3,
+      name: 'financial info II',
+      status: false
+    },
+    {
+      id: 4,
+      name: 'result',
+      status: false
+    }
+  ]
+
+  loanOptionsResult = [
+    {
+      providerName: 'Access Bank',
+      productOverview: 'Loan product for bank with some details we would like to display, talking about the loan, something like a brief description',
+      rating: '2',
+      matchingPercentage: 40,
+      image_url: 'assets/img/photos/photo-28.jpg',
+      selected: false
+    },
+    {
+      providerName: 'UBA',
+      productOverview: 'Loan product for bank with some details we would like to display, talking about the loan, something like a brief description',
+      rating: '1',
+      matchingPercentage: 20,
+      image_url: 'assets/img/photos/photo-28.jpg',
+      selected: false
+    },
+    {
+      providerName: 'Union',
+      productOverview: 'Loan product for bank with some details we would like to display, talking about the loan, something like a brief description',
+      rating: '3',
+      matchingPercentage: 60,
+      image_url: 'assets/img/photos/photo-28.jpg',
+      selected: false
+    },
+    {
+      providerName: 'GTB',
+      productOverview: 'Loan product for bank with some details we would like to display, talking about the loan, something like a brief description',
+      rating: '5',
+      matchingPercentage: 100,
+      image_url: 'assets/img/photos/photo-28.jpg',
+      selected: false
+    }
+  ]
+
   constructor(config: NgbRatingConfig) {
     // customize default values of ratings used by this component tree
     config.max = 5;
@@ -16,6 +81,40 @@ export class LoanApplicationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  searchLoan() {
+    this.loanResultDiv = true
+  }
+
+  selectLoanOption(id: number) {
+    this.loanOptionsResult[id].selected = !this.loanOptionsResult[id].selected
+    console.log('Selected status',this.loanOptionsResult[id])
+  }
+
+  prev(id: number) {
+    this.divs[id].status = false
+    this.divs[id-1].status = true
+  }
+
+  applyForLoan(id: number) {
+    this.divs[id].status = false
+    this.divs[id+1].status = true
+  }
+
+  piSubmit(id: number) {
+    this.divs[id].status = false
+    this.divs[id+1].status = true
+  }
+
+  financialInfoSubmit(id: number) {
+    this.divs[id].status = false
+    this.divs[id+1].status = true
+  }
+
+  financialInfo2Submit(id: number) {
+    this.divs[id].status = false
+    this.divs[id+1].status = true
   }
 
 }
