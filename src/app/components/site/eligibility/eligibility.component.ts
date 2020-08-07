41ca8ce5b78904bcba3ae9681d988ec03464ea7b
femibams <femibams@gmail.com>
2020-08-07 14:03:18 +0100

    updated the eligibility form to include calender for DOB

--- src/app/components/site/eligibility/eligibility.component.ts
+++ src/app/components/site/eligibility/eligibility.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbDateStruct} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-eligibility',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EligibilityComponent implements OnInit {
   active = 1
-
+  model: NgbDateStruct;
+  
   constructor() { }
 
   ngOnInit(): void {
